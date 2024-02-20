8aaee55305f3f26a17b35d46d611018e29441bd1
PlaneJane676 <pietrobalestri2004@gmail.com>
2024-02-21 00:02:31 +0100

    added bitwise rotation

--- criptData.c
+++ criptData.c
@@ -1,5 +1,25 @@
 #include "criptData.h"
 
+char rotateLeft(char c)
+{
+    char res = c<<1;
+    if(c & MSB_1BYTE)
+    {
+        res = res | LSB_1BYTE;
+    }
+    return res;
+}
+
+char rotateRight(char c)
+{
+    char res = c>>1;
+    if(c & LSB_1BYTE)
+    {
+        res = res | MSB_1BYTE;
+    }
+    return res;
+}
+
 
 void sumBasedEncryption(char* buff, unsigned long lenght, int key, int enc)
 {
--- criptData.h
+++ criptData.h
@@ -1,9 +1,14 @@
-#ifndef ARGSHANDLING
-#define ARGSHANDLING
+#ifndef CRIPT_DATA
+#define CRIPT_DATA
 
 #define ENCRYPT 1
 #define DECRYPT 0
 
+#define LSB_1BYTE 1
+#define MSB_1BYTE 0x80
+
+char rotateLeft(char c);
+char rotateRight(char c);
 void sumBasedEncryption(char* buff, unsigned long lenght, int key, int enc);
 
 #endif
